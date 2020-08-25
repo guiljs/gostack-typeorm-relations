27a60950b15dda82bfb9fb2cd0f059dd9812804b
Guilherme de Jesus Santos <guiljs@gmail.com>
2020-08-24 23:27:13 -0300

    Desenvolvendo solução do desafio

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -13,10 +13,19 @@ class OrdersRepository implements IOrdersRepository {
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
     // TODO
+
+    const order = this.ormRepository.create({
+      customer,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    return order;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    const order = await this.ormRepository.findOne(id);
+    return order;
   }
 }
 
